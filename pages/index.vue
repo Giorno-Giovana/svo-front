e66903cf2769844f061c58200db36447a4222b42
Alex Zaver <zaverclub@gmail.com>
2021-05-21 00:35:28 +0300

    Init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="map-wrap" style="height: 100vh">
+    <l-map :zoom="15" :center="[55.981317, 37.410052]" @click="log">
+      <l-tile-layer
+        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker>
+    </l-map>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      points: [],
+    }
+  },
+  methods: {
+    log(args) {
+      this.points.push(args)
+      console.log(this.points)
+    },
+  },
+}
+</script>
+
+<style></style>
