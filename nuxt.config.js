a762eca8b876bddaf07eeef675855061388c634e
Alex Zaver <zaverclub@gmail.com>
2021-05-22 02:45:48 +0300

    Добавлен atndv

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,10 +17,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ['ant-design-vue/dist/antd.css'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/antd.js'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
