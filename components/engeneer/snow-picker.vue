18a82c2453995c08eb52a1f03925629aed6f5dd0
Alex Zaver <zaverclub@gmail.com>
2021-05-22 02:49:00 +0300

    Добавлен выбор снега

---
+++ components/engeneer/snow-picker.vue
@@ -0,0 +1,37 @@
+<template>
+  <bottom-menu>
+    <div class="flex justify-between">
+      <div>Выберите объем снега</div>
+      <div>{{ snowVolumeText }}</div>
+    </div>
+    <div class="mb-10">
+      <a-slider v-model="snowVolume" :min="0" :max="10" />
+    </div>
+    <div class="flex">
+      <designed-button secondary style="margin-right: 5px" text="Назад" @click="$emit('back')" />
+      <designed-button style="margin-left: 5px" text="Далее" @click="$emit('next')" />
+    </div>
+  </bottom-menu>
+</template>
+
+<script>
+import DesignedButton from '../designed-button'
+import BottomMenu from './bottom-menu'
+export default {
+  name: 'SnowPicker',
+  components: { DesignedButton, BottomMenu },
+  data() {
+    return {
+      snowVolume: 5,
+    }
+  },
+  computed: {
+    // TODO доделать эту секцию
+    snowVolumeText() {
+      return '10-15 см'
+    },
+  },
+}
+</script>
+
+<style scoped></style>
